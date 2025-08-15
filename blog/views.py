dbd3daeb2a2ea375c3a22cf945a69790daf32b1f
Jemmal Arega <jemalarega35@gmail.com>
2025-08-15 16:51:35 +0300

    updated post get view for including pagination

--- blog/views.py
+++ blog/views.py
@@ -1,20 +1,26 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
+from rest_framework import permissions
 
 from blog.models import Post
 from blog.read_serializers import PostReadSerializer
 from blog.serializers import PostSerializers
-
+from config.pagination import CustomPagePagination
 
 class PostListView(APIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly] 
+    
     def get(self, request):
         posts = Post.objects.all().order_by("-created_at")
-        serializer = PostReadSerializer(posts, many = True)
-        return Response(serializer.data, status= status.HTTP_200_OK)
+        paginator = CustomPagePagination()
+        paginated_posts = paginator.paginate_queryset(posts, request)
+        serializer = PostReadSerializer(paginated_posts, many=True, context={"request": request})
+        return paginator.get_paginated_response(serializer.data)
+    
     
     def post(self, request):
-        serializer = PostSerializers(data = request.data)
+        serializer = PostSerializers(data = request.data, context={'request': request})
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
