0f314eb485ad97d67a714bdc7da29c41c1879ad1
Jemmal Arega <jemalarega35@gmail.com>
2025-08-15 16:49:53 +0300

    update value for related name in comment, update on delete for likes fields

--- blog/models.py
+++ blog/models.py
@@ -64,8 +64,8 @@ class Post(models.Model):
     
 class Comment(models.Model):
     id = models.BigAutoField(primary_key= True)
-    user = models.ForeignKey(User, on_delete= models.SET_NULL, related_name= 'comments')
-    post = models.ForeignKey(Post, on_delete= models.SET_NULL, related_name= 'comments')
+    user = models.ForeignKey(User, on_delete= models.CASCADE, related_name= 'comments')
+    post = models.ForeignKey(Post, on_delete= models.CASCADE, related_name= 'comments')
     content = models.TextField(max_length= 500)
     created_at = models.DateTimeField(auto_now_add= True)
     
@@ -77,8 +77,8 @@ class Comment(models.Model):
     
 class Like(models.Model):
     id = models.BigAutoField(primary_key= True)
-    user = models.ForeignKey(User, on_delete= models.SET_NULL, related_name= 'likes')
-    post = models.ForeignKey(Post, on_delete= models.SET_NULL, related_name= 'likes')
+    user = models.ForeignKey(User, on_delete= models.CASCADE, related_name= 'likes')
+    post = models.ForeignKey(Post, on_delete= models.CASCADE, related_name= 'likes')
     created_at = models.DateTimeField(auto_now_add= True)
     
     class Meta:
