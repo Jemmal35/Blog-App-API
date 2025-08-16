619e320e498b666b46593ce6451a4dd3c1b8879d
Jemmal Arega <jemalarega35@gmail.com>
2025-08-16 13:51:06 +0300

    Created an endpoint for tag api

--- blog/views.py
+++ blog/views.py
@@ -58,6 +58,58 @@ class CategoryDetailApiView(APIView):
         category.delete()
         return Response({"message": "Category deleted successfully."}, status= status.HTTP_204_NO_CONTENT)
 
+
+class TagApiView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+    
+    def get(self, request):
+        tag = Tag.objects.all().order_by('name')
+        serializer = TagSerializer(tag, many = True)
+        return Response(serializer.data, status = status.HTTP_200_OK)
+    
+    
+    def post(self, request):
+        serializer = TagSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status= status.HTTP_201_CREATED)
+        return Response(serializer.errors, status= status.HTTP_400_BAD_REQUEST)
+
+    
+class TagDetailApiView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+       
+    def get_object(self, id):
+        try:
+            tag = Tag.objects.get(pk = id)
+            return tag
+        except Tag.DoesNotExist:
+            return None
+        
+    def get(self, request, id):
+        tag = self.get_object(id)
+        if not tag:
+            return Response({"error":"Tag not found."}, status=status.HTTP_404_NOT_FOUND)
+        serializer = TagSerializer(tag)
+        return Response(serializer.data, status= status.HTTP_200_OK)
+    
+    def put(self, request, id):
+        tag = self.get_object(id)
+        if not tag:
+            return Response({"error":"Tag not found."}, status=status.HTTP_404_NOT_FOUND)
+        serializer = TagSerializer(tag, data = request.data, partial = True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status= status.HTTP_200_OK)
+        return Response(serializer.errors, status= status.HTTP_400_BAD_REQUEST)
+    def delete(self, request, id):
+        tag = self.get_object(id)
+        if not tag:
+            return Response({"error":"Tag not found."}, status=status.HTTP_404_NOT_FOUND)
+        tag.delete()
+        return Response({"message": "Tag deleted successfully."}, status= status.HTTP_204_NO_CONTENT)
+
+
 class PostListView(APIView):
     permission_classes = [permissions.IsAuthenticatedOrReadOnly] 
     
