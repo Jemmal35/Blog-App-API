673e83a35255f34c4608b27c62706ef17ce301c1
Jemmal Arega <jemalarega35@gmail.com>
2025-08-16 13:55:07 +0300

    created urls for the tag endpoints

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from .views import PostListView, PostDetailView
+from .views import PostListView, PostDetailView, CategoryApiView, CategoryDetailApiView, TagApiView, TagDetailApiView
 
 
 urlpatterns = [
     path("api/v1/posts/",PostListView.as_view(), name='post-list'),
-    path("api/v1/posts/<id>/",PostDetailView.as_view(), name='post-detail')
+    path("api/v1/posts/<id>/",PostDetailView.as_view(), name='post-detail'),
+    path("api/v1/category/", CategoryApiView.as_view(), name= 'category'),
+    path("api/v1/category/<id>/", CategoryApiView.as_view(), name= 'category-detail'),
+    path("api/v1/tag/", CategoryApiView.as_view(), name= 'tag'),
+    path("api/v1/tag/<id>/", CategoryApiView.as_view(), name= 'tag-detail'),   
     
 ]
