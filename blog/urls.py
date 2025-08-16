5362410a3c6c917047d249e4f50c09f0b445a834
Jemmal Arega <jemalarega35@gmail.com>
2025-08-16 14:09:24 +0300

    Updated the view for the urls

--- blog/urls.py
+++ blog/urls.py
@@ -6,8 +6,8 @@ urlpatterns = [
     path("api/v1/posts/",PostListView.as_view(), name='post-list'),
     path("api/v1/posts/<id>/",PostDetailView.as_view(), name='post-detail'),
     path("api/v1/category/", CategoryApiView.as_view(), name= 'category'),
-    path("api/v1/category/<id>/", CategoryApiView.as_view(), name= 'category-detail'),
-    path("api/v1/tag/", CategoryApiView.as_view(), name= 'tag'),
-    path("api/v1/tag/<id>/", CategoryApiView.as_view(), name= 'tag-detail'),   
+    path("api/v1/category/<id>/", CategoryDetailApiView.as_view(), name= 'category-detail'),
+    path("api/v1/tag/", TagApiView.as_view(), name= 'tag'),
+    path("api/v1/tag/<id>/", TagDetailApiView.as_view(), name= 'tag-detail'),   
     
 ]
