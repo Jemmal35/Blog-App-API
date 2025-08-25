19e15fc219f8f29966f75546e4a5be4781e48bb1
Jemmal Arega <jemalarega35@gmail.com>
2025-08-25 10:29:20 +0300

    Created urls for logout view

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from .views import UserRegistrationView, UserProfileView, UserProfileUpdateView, UpdatePasswordView
+from .views import LogoutAPIView, UserRegistrationView, UserProfileView, UserProfileUpdateView, UpdatePasswordView
 
 urlpatterns = [
     path("api/v1/register/", UserRegistrationView.as_view(), name= 'registration'),
     path("api/v1/login/",TokenObtainPairView.as_view(), name= 'login'),
+    path("api/v1/logout/", LogoutAPIView.as_view(), name="logout"),
     path("api/v1/refresh/", TokenRefreshView.as_view(), name= 'refresh'),
     path("api/v1/profile/", UserProfileView.as_view(), name= 'user-profile'),
     path("api/v1/profile/update/", UserProfileUpdateView.as_view(), name= 'user-profile-update'),
